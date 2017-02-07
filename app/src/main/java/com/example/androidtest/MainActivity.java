9167f1cc4ba30b1e82135caf354e0118f1ac4d1c
InefficientCode <InefficientCode@users.noreply.github.com>
2017-02-07 20:06:15 +0300

    Initial commit;

---
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.androidtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileCenter.start(getApplication(), "{Your App Secret}", Analytics.class, Crashes.class);
+
+        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Analytics.trackEvent("Analytics Event Button Clicked!");
+            }
+        });
+
+        ((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Crashes.generateTestCrash();
+            }
+        });
+    }
+}
